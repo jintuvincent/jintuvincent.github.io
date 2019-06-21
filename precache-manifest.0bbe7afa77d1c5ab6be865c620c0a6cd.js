71d481ed279a15aef078d28706e72a2258bdf9ee
jintuvincent <jintuvincent@users.noreply.github.com>
2019-06-21 19:39:16 +0530

    updating

---
+++ precache-manifest.0bbe7afa77d1c5ab6be865c620c0a6cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "731376cb5e6681583a34089abfc338a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86ba1a9d2b5b1bc023df",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "0974566bb1ba1c8e257e",
+    "url": "/static/css/main.89632dd9.chunk.css"
+  },
+  {
+    "revision": "86ba1a9d2b5b1bc023df",
+    "url": "/static/js/2.7ca30bdf.chunk.js"
+  },
+  {
+    "revision": "0974566bb1ba1c8e257e",
+    "url": "/static/js/main.6af23465.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
